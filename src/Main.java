a9b374682801c1d8b909282459bcf54fea910295
JoFyDo <JoFyDo@users.noreply.github.com>
2020-12-29 00:07:36 +0100

    cvgfhb

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Spiel s = new Spiel();
-        SpielManger sm = new SpielManger(s, new String[]{"Zocker 1"}, true);
+        SpielManger sm = new SpielManger(s, new String[]{"Schorsch"}, true);
         sm.startSpiel();
 
     }
